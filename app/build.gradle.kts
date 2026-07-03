c37911bc66feb4e083765c6975b789531856f6eb
lucas <lucasriverar2001@gmail.com>
2026-07-02 21:44:50 -0500

    FEATURE: Add firebase firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,4 +77,5 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-crashlytics")
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore")
 }
---
+++ app/src/main/java/com/lucasdev/proxi/di/FirestoreModule.kt
@@ -0,0 +1,16 @@
+package com.lucasdev.proxi.di
+
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirestoreModule {
+
+    @Provides
+    fun provideFirestore() = Firebase.firestore
+}
